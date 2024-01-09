c41d77845a1b2956947390a1c848e307be49a9c7
Mitchell Robert Vollger <mrvollger@gmail.com>
2024-01-08 20:31:31 -0800

    update

--- scripts/fire-correlations.sh
+++ scripts/fire-correlations.sh
@@ -6,6 +6,7 @@ printf "#chrom\tstart\tend\tGM12878\tGM12878_30X\tPS00338\tPS00356\tDNase\n" >".
 
 bedops --ec -m <(zcat $a) <(zcat $b) <(zcat $c) |
     bedmap --ec --echo --max --delim '\t' - <(hck -z -f 1-4 -F score results/GM12878/FDR-peaks/FDR.track.bed.gz) |
+    bedmap --ec --echo --max --delim '\t' - <(hck -z -f 1-4 -F score results/30X_GM12878/FDR-peaks/FDR.track.bed.gz) |
     bedmap --ec --echo --max --delim '\t' - <(hck -z -f 1-4 -F score results/PS00338_COLO829BL_1/FDR-peaks/FDR.track.bed.gz) |
     bedmap --ec --echo --max --delim '\t' - <(hck -z -f 1-4 -F score results/PS00356_COLO829BL_2/FDR-peaks/FDR.track.bed.gz) |
     bedmap --ec --echo --max --delim '\t' - <(bioawk -t '{print $1,$2,$3,".",$4}' data/bedgraph_annotations/ENCFF960FMM_dnase_signal.bed) \
